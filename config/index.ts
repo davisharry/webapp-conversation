f6749d9439a68d90348846714fd43f35ce56f54e
Vincent <vincent@heistech.com>
2023-05-30 15:56:15 +0800

    添加配置，修改页面

--- config/index.ts
+++ config/index.ts
@@ -1,19 +1,19 @@
 import { AppInfo } from "@/types/app"
 export const APP_ID = '42c3c2b7-5cd9-4e68-9251-7b59a50e1d08'
-export const API_KEY = ''
+export const API_KEY = 'app-DxIlgNcfWjvnulGHPN5e7VQV'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  "title": 'Chat APP',
-  "description": '',
-  "copyright": '',
-  "privacy_policy": '',
+  "title": 'Heistech',
+  "description": '1111',
+  "copyright": 'copyright@heistech co',
+  "privacy_policy": 'privacy policy',
   "default_language": 'zh-Hans'
 }
 
 export const isShowPrompt = false
 export const promptTemplate = 'I want you to act as a javascript console.'
 
-export const API_PREFIX = '/api';
+export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
--- i18n/lang/app.en.ts
+++ i18n/lang/app.en.ts
@@ -8,7 +8,7 @@ const translation = {
     newChat: "New chat",
     newChatDefaultName: "New conversation",
     openingStatementTitle: "Opening statement",
-    powerBy: "Powered by",
+    powerBy: "Powered by Heistech",
     prompt: "Prompt",
     privatePromptConfigTitle: "Conversation settings",
     publicPromptConfigTitle: "Initial Prompt",
